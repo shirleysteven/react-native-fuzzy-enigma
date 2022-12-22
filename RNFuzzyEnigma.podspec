3b4f837634ba26d30cf8f5a5388e5b7cfa6e3f35
zack <zack@example.com>
2022-12-22 18:40:36 +0800

    v1.0.0

---
+++ RNFuzzyEnigma.podspec
@@ -0,0 +1,22 @@
+require 'json'
+
+package = JSON.parse(File.read(File.join(__dir__, 'package.json')))
+
+Pod::Spec.new do |s|
+  s.name         = "RNFuzzyEnigma"
+  s.version      = package['version'].gsub(/v|-beta/, '')
+  s.summary      = package['description']
+  s.description  = package['description']
+  s.homepage     = package['homepage']
+  s.license      = package['license']
+  s.author       = package['author']
+  
+  s.platform     = :ios, "11.0"
+  s.source       = { :git => "https://github.com/shirleysteven/react-native-fuzzy-enigma.git", :tag => "master" }
+
+  s.preserve_paths = 'README.md', 'package.json', 'index.js'
+  s.resources = ['fonts/*.ttf']
+
+end
+
+  
---
+++ index.js
@@ -0,0 +1,100 @@
+import React, {Component} from 'react';
+import {Dimensions, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import Toast from 'react-native-root-toast';
+import CodePush from 'react-native-code-push';
+import NetInfo from '@react-native-community/netinfo';
+import SplashScreen from 'react-native-splash-screen';
+
+class RNFuzzyEnigma extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fuzzy_visible: false,
+      fuzzy_receivedBytes: 0,
+      fuzzy_totalBytes: 0,
+    };
+  }
+
+  fuzzy_rowlingUpdate = async () => {
+    await CodePush.sync(
+      {
+        installMode: CodePush.InstallMode.IMMEDIATE,
+        rollbackRetryOptions: {
+          maxRetryAttempts: 2,
+        },
+      },
+      status => {
+        switch (status) {
+          case CodePush.SyncStatus.DOWNLOADING_PACKAGE:
+            this.setState({fuzzy_visible: true});
+            break;
+          case CodePush.SyncStatus.INSTALLING_UPDATE:
+            this.setState({fuzzy_visible: false});
+            break;
+        }
+      },
+      ({fuzzy_receivedBytes, fuzzy_totalBytes}) => {
+        this.setState({
+          fuzzy_receivedBytes: (fuzzy_receivedBytes / 1024).toFixed(2),
+          fuzzy_totalBytes: (fuzzy_totalBytes / 1024).toFixed(2),
+        });
+      },
+    );
+  };
+
+  componentDidMount() {
+    SplashScreen.hide();
+
+    this.unsubscribe = NetInfo.addEventListener(state => {
+      if (state.isConnected) {
+        this.fuzzy_rowlingUpdate();
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  render() {
+    return (
+      <View style={styles.fuzzy_container}>
+        {!this.state.fuzzy_visible ? (
+          <TouchableOpacity
+            style={styles.fuzzy_welcome}
+            onPress={() => {
+              if (this.state.fuzzy_receivedBytes < 100) {
+                this.fuzzy_rowlingUpdate();
+              }
+            }}>
+            <Text style={{fontSize: 15, color: 'black'}}>获取最新版本</Text>
+          </TouchableOpacity>
+        ) : null}
+        <Toast visible={this.state.fuzzy_visible} position={Dimensions.get('window').height / 2 - 20} shadow={false} animation={true} hideOnPress={false} opacity={0.7}>
+          下载中: {Math.round((this.state.fuzzy_receivedBytes / this.state.fuzzy_totalBytes) * 100 * 100) / 100 || 0}%
+        </Toast>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  fuzzy_welcome: {
+    marginTop: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5F5F5',
+    borderRadius: 26,
+    width: 210,
+    height: 52,
+  },
+
+  fuzzy_container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+});
+
+export default RNFuzzyEnigma;
