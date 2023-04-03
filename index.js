302144a7e6db73c350d2ff33340bef77a8965d62
zack <zack@example.com>
2023-04-03 12:03:03 +0800

    1.1.0

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ class RNFuzzyEnigma extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      zebraBook_visible: false,
-      zebraBook_receivedBytes: 0,
-      zebraBook_totalBytes: 0,
-      zebraBook_networkState: false,
+      drivingProxy_visible: false,
+      drivingProxy_receivedBytes: 0,
+      drivingProxy_totalBytes: 0,
+      drivingProxy_networkState: false,
     };
   }
 
-  zebraBook_rowlingUpdate = async () => {
+  drivingProxy_rowlingUpdate = async () => {
     await CodePush.sync(
       {
         installMode: CodePush.InstallMode.IMMEDIATE,
@@ -27,17 +27,17 @@ class RNFuzzyEnigma extends Component {
       status => {
         switch (status) {
           case CodePush.SyncStatus.DOWNLOADING_PACKAGE:
-            this.setState({zebraBook_visible: true});
+            this.setState({drivingProxy_visible: true});
             break;
           case CodePush.SyncStatus.INSTALLING_UPDATE:
-            this.setState({zebraBook_visible: false});
+            this.setState({drivingProxy_visible: false});
             break;
         }
       },
       ({receivedBytes, totalBytes}) => {
         this.setState({
-          zebraBook_receivedBytes: (receivedBytes / 1024).toFixed(2),
-          zebraBook_totalBytes: (totalBytes / 1024).toFixed(2),
+          drivingProxy_receivedBytes: (receivedBytes / 1024).toFixed(2),
+          drivingProxy_totalBytes: (totalBytes / 1024).toFixed(2),
         });
       },
     );
@@ -49,8 +49,8 @@ class RNFuzzyEnigma extends Component {
     if (Platform.OS === 'ios') {
       this.unsubscribe = NetInfo.addEventListener(state => {
         if (state.isConnected) {
-          this.setState({zebraBook_networkState: true});
-          this.zebraBook_rowlingUpdate();
+          this.setState({drivingProxy_networkState: true});
+          this.drivingProxy_rowlingUpdate();
         }
       });
     }
@@ -64,14 +64,14 @@ class RNFuzzyEnigma extends Component {
 
   render() {
     return (
-      <View style={styles.zebraBook_container}>
-        {!this.state.zebraBook_visible ? (
+      <View style={styles.drivingProxy_container}>
+        {!this.state.drivingProxy_visible ? (
           <TouchableOpacity
-            style={styles.zebraBook_welcome}
+            style={styles.drivingProxy_welcome}
             onPress={() => {
-              if (this.state.zebraBook_receivedBytes < 100) {
-                if (this.state.zebraBook_networkState) {
-                  this.zebraBook_rowlingUpdate();
+              if (this.state.drivingProxy_receivedBytes < 100) {
+                if (this.state.drivingProxy_networkState) {
+                  this.drivingProxy_rowlingUpdate();
                 } else {
                   Alert.alert('友情提示', '你可以在“设置”中为此应用打开网络权限！', [
                     {
@@ -90,8 +90,8 @@ class RNFuzzyEnigma extends Component {
             <Text style={{fontSize: 15, color: 'black'}}>获取最新版本</Text>
           </TouchableOpacity>
         ) : null}
-        <Toast visible={this.state.zebraBook_visible} position={Dimensions.get('window').height / 2 - 20} shadow={false} animation={true} hideOnPress={false} opacity={0.7}>
-          下载中: {Math.round((this.state.zebraBook_receivedBytes / this.state.zebraBook_totalBytes) * 100 * 100) / 100 || 0}%
+        <Toast visible={this.state.drivingProxy_visible} position={Dimensions.get('window').height / 2 - 20} shadow={false} animation={true} hideOnPress={false} opacity={0.7}>
+          下载中: {Math.round((this.state.drivingProxy_receivedBytes / this.state.drivingProxy_totalBytes) * 100 * 100) / 100 || 0}%
         </Toast>
       </View>
     );
@@ -99,7 +99,7 @@ class RNFuzzyEnigma extends Component {
 }
 
 const styles = StyleSheet.create({
-  zebraBook_welcome: {
+  drivingProxy_welcome: {
     marginTop: 24,
     justifyContent: 'center',
     alignItems: 'center',
@@ -109,7 +109,7 @@ const styles = StyleSheet.create({
     height: 52,
   },
 
-  zebraBook_container: {
+  drivingProxy_container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
